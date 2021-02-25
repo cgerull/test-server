1e3d555887eb2c3eefb0ee2a7963858b2c47c67f
Claus Gerull <cgerull@live.com>
2021-02-25 10:52:23 +0100

    Add read_config tests.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,6 +6,17 @@ from app import routes
 def test_get_secret_key():
     assert routes.get_secret_key() == 'Only_the_default_secret_key'
 
+def test_read_config_no_file():
+    config_items = len(routes.srv_config)
+    routes.read_config(routes.config_file, routes.srv_config)
+    assert config_items == len(routes.srv_config)
+
+def test_read_config_with_file():
+    config_items = len(routes.srv_config)
+    routes.read_config('example-srv-config.yml', routes.srv_config)
+    assert config_items != len(routes.srv_config)
+    assert 11 == len(routes.srv_config)
+
 
 # def test_get_secret_key_from_file(tmpdir):
 #     file = 'mysecret'
