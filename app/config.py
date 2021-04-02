cb7327ae0bab11c754ba19656e9f796c7f94dd30
Claus <cgerull@live.com>
2021-04-02 12:35:25 +0200

    Add redis configuration. (#15)
    * Add redis configuration.
    
    * Refine Redis configuration.

--- app/config.py
+++ app/config.py
@@ -3,12 +3,25 @@ import os
 
 class Config(object):
     """Read and set configuration values"""
-    SECRET_KEY = 'Only_the_default_secret_key'
-    SECRET_FILE = '/run/secrets/my_secret_key'
+    DEBUG = False
+    TESTING = False
 
+    APP_NAME = 'Test server'
+    APP_FOOTER = os.environ.get('APP_FOOTER') or 'Default configuration'
+
+    SECRET_FILE = '/run/secrets/my_secret_key'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Only_the_default_secret_key'
     CONFIG_FILE = './srv-config.yml'
-
+    
+    ACCESS_LOG = os.environ.get('ACCESS_LOG') or None
+    ERROR_LOG = os.environ.get('ERROR_LOG') or None
+    LOG_LINES = 30
+    
+    REDIS_URL = os.environ.get("REDIS_URL") or None
+    REDIS_SERVER = os.environ.get('REDIS_SERVER') or None
+    REDIS_PORT = os.environ.get('REDIS_PORT') or 6379
+    REDIS_HTML_COUNTER = 'api_srv_html_counter'
+    REDIS_API_COUNTER = 'api_srv_counter'
 
     # def get_secret_file():
     #     return  _SECRET_FILE
