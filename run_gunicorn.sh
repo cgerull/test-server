34d8720eacc09d6291ce952a2b364d551e6e76b5
Claus <cgerull@live.com>
2021-04-29 17:07:41 +0200

    Add default values. (#18)

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
     gunicorn wsgi:app \
-    --bind="0.0.0.0:${PORT}" \
+    --bind="0.0.0.0:5000" \
     --workers=2 \
-    --access-logformat="${LOGFORMAT}" \
+    --access-logformat="%(h)s %(l)s %(t)s %({Server-IP}o)s %(l)s %(r)s %(s)s %(b)s %(a)s" \
     --access-logfile="-" \
     --error-logfile="-" \
-    --log-level="${LOGLVL}"
+    --log-level="INFO"
