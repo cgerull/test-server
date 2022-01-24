827458d3c354e9f8d05522bb82318657a2e9baa4
Claus Gerull <claus.gerull@solvinity.com>
2022-01-24 16:29:02 +0100

    Set gunicorn worker timeout.

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -3,6 +3,8 @@
     gunicorn wsgi:app \
     --bind="0.0.0.0:8080" \
     --workers=2 \
+    # --worker-class=gevent \
+    --timeout=90 \
     --access-logformat="%(h)s %(l)s %(t)s %({Server-IP}o)s %(l)s %(r)s %(s)s %(b)s %(a)s" \
     --access-logfile="-" \
     --error-logfile="-" \
