82cb4c3f63f172669d4d8c2186ef8c8eba9b7b80
Claus Gerull <cgerull@live.com>
2022-03-18 19:40:07 +0100

    Exclude health endpoint from metrics.

--- test_server/__init__.py
+++ test_server/__init__.py
@@ -56,6 +56,7 @@ def create_app(test_config=None):
 
     # A simple page that gives the health status
     @app.route('/health')
+    @metrics.do_not_track()
     def health():
         return "{} is healthy.".format(__name__)
 
