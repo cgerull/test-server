c8d0c49f3eaa778d323d1e933a1b121af7aecfee
Claus Gerull <claus.gerull@solvinity.com>
2022-05-12 09:15:08 +0200

    Remove gevent because of problems with alpine.

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -7,12 +7,12 @@ export DB_TYPE=sqlite
 flask init-db 
 
 # Start the application server
+#     --worker-class=gevent \
 NUM_WORKERS=2
 TIMEOUT=120
 gunicorn wsgi:app \
     --bind="0.0.0.0:8080" \
     --workers="${NUM_WORKERS}" \
-    --worker-class=gevent \
     --timeout="${TIMEOUT}" \
     --access-logformat="%(h)s %(l)s %(t)s %({Server-IP}o)s %(l)s %(r)s %(s)s %(b)s %(a)s" \
     --access-logfile="-" \
