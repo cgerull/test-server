2afb46272ecebf2d035889a54ec2526653a2dab8
Claus <cgerull@live.com>
2022-05-18 08:39:16 +0200

    Update status info and page permissions. (#43)

--- test_server/status.py
+++ test_server/status.py
@@ -17,6 +17,7 @@ import socket
 from flask import (
     Blueprint, current_app, render_template, request
 )
+from test_server.local_data import LocalData
 # from flask.helpers import url_for
 # from werkzeug.utils import redirect
 
@@ -30,60 +31,74 @@ bp = Blueprint('status', __name__, url_prefix='/')
 
 
 @bp.route('/status', methods=['GET'])
+# @login_required
 def status():
     """Build response data and send page to requester."""
-    response_data = build_response_data()
+    # response_data = build_response_data()
+    local_data = LocalData()
+    error = None
 
 
-    return render_template('status/status.html',resp=response_data)
+    # return render_template('status/status.html',resp=response_data)
+    if error:
+        flash(error)
 
+    return render_template('status/status.html',
+                server_info=local_data.get_server_info(),
+                server_state=local_data.get_server_state(),
+                # state=local_data.get_server_state(),
+                env={
+                        'version': current_app.config['VERSION'],
+                        'environment': current_app.config['ENV']
+                    },
+                )
 #
 # Utiliy functions
 # ############################################################################
-def build_response_data():
-    """
-    Build a dictionary with timestamp, server ip,
-    server name, secret and requester ip.
-    """
-    hostname = socket.gethostname()
+# def build_response_data():
+#     """
+#     Build a dictionary with timestamp, server ip,
+#     server name, secret and requester ip.
+#     """
+#     hostname = socket.gethostname()
 
-    return {
-        'now': datetime.now().isoformat(sep=' '),
-        'platform': platform.platform(),
-        'os_version': platform.version(),
-        'load_average': os.system("uptime | cut -f 4 -d ,"),
-        'hostname' : hostname
-    }
+#     return {
+#         'now': datetime.now().isoformat(sep=' '),
+#         'platform': platform.platform(),
+#         'os_version': platform.version(),
+#         'load_average': os.system("uptime | cut -f 4 -d ,"),
+#         'hostname' : hostname
+#     }
 
-def get_remote_ip():
-    """
-    Get client ip address, trying to resolve any
-    proxies that modify the request.
-    """
-    client_ip = ''
-    if 'HTTP_X_REAL_IP' in request.environ :
-        client_ip = request.environ['HTTP_X_REAL_IP']
-    elif 'X_REAL_IP' in request.environ :
-        client_ip = request.environ['X_REAL_IP']
-    elif 'HTTP_X_FORWARDED_FOR' in request.environ :
-        client_ip = request.environ['HTTP_X_FORWARDED_FOR']
-    else:
-        client_ip = request.remote_addr
+# def get_remote_ip():
+#     """
+#     Get client ip address, trying to resolve any
+#     proxies that modify the request.
+#     """
+#     client_ip = ''
+#     if 'HTTP_X_REAL_IP' in request.environ :
+#         client_ip = request.environ['HTTP_X_REAL_IP']
+#     elif 'X_REAL_IP' in request.environ :
+#         client_ip = request.environ['X_REAL_IP']
+#     elif 'HTTP_X_FORWARDED_FOR' in request.environ :
+#         client_ip = request.environ['HTTP_X_FORWARDED_FOR']
+#     else:
+#         client_ip = request.remote_addr
 
-    return client_ip
+#     return client_ip
 
-def get_secret_key():
-    """
-    Return secret key from:
-        Docker secret file or
-        Environment variable SECRET_KEY or
-        a default value
-    """
-    secret = ''
-    try:
-        with open(current_app.config['SECRET_FILE'], mode = 'r', encoding = 'utf_8') as file:
-            secret = file.read()
-    except IOError:
-        # no file, return configured secret
-        secret = current_app.config['SECRET_KEY']
-    return secret
+# def get_secret_key():
+#     """
+#     Return secret key from:
+#         Docker secret file or
+#         Environment variable SECRET_KEY or
+#         a default value
+#     """
+#     secret = ''
+#     try:
+#         with open(current_app.config['SECRET_FILE'], mode = 'r', encoding = 'utf_8') as file:
+#             secret = file.read()
+#     except IOError:
+#         # no file, return configured secret
+#         secret = current_app.config['SECRET_KEY']
+#     return secret
