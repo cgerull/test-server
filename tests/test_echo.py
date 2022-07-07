4cddfd2b947ef5a9fd95ea6511591d60a76690a5
Claus Gerull <cgerull@live.com>
2022-07-07 16:41:24 +0200

    Rebased with master.

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -1,10 +1,14 @@
-import pytest
+"""
+Test echo page
+"""
+# import pytest
 # from test_server.db import get_db
 
 def test_echo_response(client):
+    """ Happy flow, echo page."""
     assert client.get('/echo').status_code == 200
 
 
-
 def test_echo_index_response(client):
+    """ Happy flow, index redirect."""
     assert client.get('/').status_code == 302
