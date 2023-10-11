927d4c4149dad7c34cda92fd67a0a690c2426246
Claus Gerull <cgerull@live.com>
2023-10-11 10:13:28 +0200

    TS-1 Fix encoding field.

--- test_server/echo_data.py
+++ test_server/echo_data.py
@@ -19,7 +19,6 @@ class EchoData():
     headers = {}
 
     def __init__(self, my_request):
-        # self.local_data = self.set_local_data()
         self.remote_data = self.set_remote_data(my_request)
 
     def get_remote_data(self):
@@ -28,11 +27,10 @@ class EchoData():
 
     def set_remote_data(self, my_request):
         """ Set class data from http request."""
-        # self.remote_data = my_request.headers
         return {
             'remote_addr': self.get_remote_ip(my_request),
             'url': my_request.url,
-            'url_charset': my_request.url_charset,
+            'url_charset': my_request.content_encoding,
             'referrer': my_request.referrer,
             'user_agent': my_request.user_agent.string,
         }
@@ -40,11 +38,10 @@ class EchoData():
     @staticmethod
     def get_http_headers(my_request):
         """ Get HTTP headers from request."""
-        # self.headers = self.get_remote_data(my_request)
         result = {}
-        for header in my_request.headers.envrion:
+        for header in my_request.headers.environ:
             if header.startswith('HTTP_'):
-                result[header]=my_request.headers.envrion[header]
+                result[header]=my_request.headers.environ[header]
 
         return result
 
