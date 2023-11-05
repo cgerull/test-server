7fbbfbef779afe1bcf0a58311c30d4524dc062db
Claus Gerull <cgerull@live.com>
2023-11-05 14:20:05 +0100

    TS-4 Reoganize code.

--- test_server/health.py
+++ test_server/health.py
@@ -6,7 +6,7 @@ Check app health endpoint.
 from flask import (
     Blueprint, current_app
 )
-from test_server.db import get_db
+from test_server.db import check_database
 from test_server.persistent_counter import check_redis
 
 bp = Blueprint('health', __name__, url_prefix='/')
@@ -18,37 +18,11 @@ def health():
     health_msg = "testserver is healthy."
     status = 200
 
-    err = check_database() or check_redis(current_app.config['REDIS_SERVER'])
+    db_err = check_database()
+    redis_err = check_redis(current_app.config['REDIS_SERVER'])
 
-    if err:
-        health_msg = err
+    if not db_err['pass'] or not redis_err['pass']:
+        health_msg = f"(DB: {db_err['msg']}; Redis: {redis_err['msg']})"
         status = 500
 
     return health_msg, status
-
-def check_database():
-    '''
-    Test if the configured database is accessible and configured.
-    '''
-    status = ""
-    my_db= get_db()
-    if my_db is not None:
-        try:
-            my_cursor = my_db.cursor()
-            my_cursor.execute(
-                "SELECT * from req_log"
-            )
-        except my_db.OperationalError as err:
-            status = f"ERROR! Can't connect to database. {err}"
-    return status
-
-# def check_redis():
-#     '''
-#     Test if the configured redis is accessible.
-#     '''
-#     status = ""
-
-
-#     return status
-
-# Add tests when future features will be developed.
--- test_server/persistent_counter.py
+++ test_server/persistent_counter.py
@@ -74,20 +74,30 @@ def check_redis(host, key='check_point', port=6379, password=None):
     Open a connection to redis, then write, read and delete
     a KV pair to ensure Redis is configured and alive.
     '''
-    result = ""
+    # result = ""
+    result = {
+        "pass": True,
+        "msg": "OK"
+    }
     my_key = key
     if host:
         try:
-            print(f"Connect to Redis service on {host}.")
+            print(f"Connecting to Redis service on {host}.")
             redis_connection = redis.Redis(
                 host = host,
                 port = port,
-                password = password
+                password = password,
+                socket_connect_timeout = 5
                 )
             redis_connection.set(my_key, datetime.now().isoformat(sep=' '))
             redis_connection.get(my_key)
             redis_connection.delete(my_key)
         except redis.ConnectionError as err:
-            result = f"Error creating Redis connection: {err}"
+            result['pass'] = False
+            result['msg'] = f"Error creating Redis connection: {err}"
+            redis_connection = None
+        except redis.TimeoutError as err:
+            result['pass'] = False
+            result['msg'] = f"Error: {err}"
             redis_connection = None
     return result
