401f94bec459d0c4dc89a61d18d834f0fa0f81e9
Claus Gerull <cgerull@live.com>
2023-11-05 21:39:57 +0100

    TS-15 Refactor EchoData class.

--- test_server/echo.py
+++ test_server/echo.py
@@ -63,7 +63,7 @@ def echo():
                 "INSERT INTO \
                     req_log (response_code, request_url, request_from_ip) \
                     VALUES (?, ?, ?)",
-                (200, request.url, remote_info['remote_addr']),
+                (200, request.url, remote_info['RemoteAddr']),
             )
             my_db.commit()
         except my_db.IntegrityError:
--- test_server/echo_data.py
+++ test_server/echo_data.py
@@ -19,25 +19,34 @@ class EchoData():
     headers = {}
 
     def __init__(self, my_request):
-        self.remote_data = self.set_remote_data(my_request)
+        # self.remote_data = self.set_remote_data(my_request)
         self.request = my_request
 
     def get_remote_data(self):
         """ Return the class data set."""
-        return self.remote_data
-
-    def set_remote_data(self, my_request):
-        """ Set class data from http request."""
         return {
-            'remote_addr': self.get_remote_ip(my_request),
-            'url': my_request.url,
-            'url_charset': my_request.charset,
-            'accept_encodings': my_request.accept_encodings,
-            'accept_languages': my_request.accept_languages,
-            'accept_mimetypes': my_request.accept_mimetypes,
-            'referrer': my_request.referrer,
-            'user_agent': my_request.user_agent.string,
+            'RemoteAddr': self.get_remote_ip(),
+            'URL': self.request.url,
+            'Charset': self.request.charset,
+            'Cookies': self.request.cookies,
+            'Method': f"{self.request.environ['REQUEST_METHOD']} {self.request.environ['SERVER_PROTOCOL']}",
+            'QueryString': self.request.environ['QUERY_STRING'],
+            'Referrer': self.request.referrer,
+            'UserAgent': self.request.user_agent.string,
         }
+
+    # def set_remote_data(self, my_request):
+    #     """ Set class data from http request."""
+    #     return {
+    #         'remote_addr': self.get_remote_ip(my_request),
+    #         'url': my_request.url,
+    #         'url_charset': my_request.charset,
+    #         'accept_encodings': my_request.accept_encodings,
+    #         'accept_languages': my_request.accept_languages,
+    #         'accept_mimetypes': my_request.accept_mimetypes,
+    #         'referrer': my_request.referrer,
+    #         'user_agent': my_request.user_agent.string,
+    #     }
 
     # @staticmethod
     def get_http_headers(self):
@@ -49,20 +58,20 @@ class EchoData():
 
         return result
 
-    @staticmethod
-    def get_remote_ip(my_request):
+    # @staticmethod
+    def get_remote_ip(self):
         """
         Get client ip address, trying to resolve any
-        proxies that modify the my_request.
+        proxies that modify the request.
         """
         client_ip = ''
-        if 'HTTP_X_REAL_IP' in my_request.environ :
-            client_ip = my_request.environ['HTTP_X_REAL_IP']
-        elif 'X_REAL_IP' in my_request.environ :
-            client_ip = my_request.environ['X_REAL_IP']
-        elif 'HTTP_X_FORWARDED_FOR' in my_request.environ :
-            client_ip = my_request.environ['HTTP_X_FORWARDED_FOR']
+        if 'HTTP_X_REAL_IP' in self.request.environ :
+            client_ip = self.request.environ['HTTP_X_REAL_IP']
+        elif 'X_REAL_IP' in self.request.environ :
+            client_ip = self.request.environ['X_REAL_IP']
+        elif 'HTTP_X_FORWARDED_FOR' in self.request.environ :
+            client_ip = self.request.environ['HTTP_X_FORWARDED_FOR']
         else:
-            client_ip = my_request.remote_addr
+            client_ip = self.request.remote_addr
 
         return client_ip
